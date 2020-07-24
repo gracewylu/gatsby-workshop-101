af2160bc9405c09aeaf567cec83deddb747ddea9
Grace Lu <grace@codehs.com>
2020-07-24 15:20:44 -0400

    Add blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,28 @@
 module.exports = {
-    plugins: [`gatsby-plugin-mdx`],
+    siteMetadata: {
+        title: `Grace Lu`,
+        author: `Grace Lu`,
+        description: `Grace's personal site for Gatsby 101 at CodeLand`,
+        siteUrl: `https://gl-workshop-101-site.netlify.app/`,
+        social: [
+            {
+                name: `LinkedIn`,
+                url: `https://www.linkedin.com/in/grace-lu-3a939586/`,
+            },
+            {
+                name: `GitHub`,
+                url: `https://github.com/gracewylu`,
+            },
+        ],
+    },
+    plugins: [
+        `gatsby-plugin-mdx`,
+        {
+            resolve: `gatsby-theme-blog`,
+            options: {
+                basePath: `/blog`,
+                mdxOtherwiseConfigured: true,
+            },
+        },
+    ],
 };
